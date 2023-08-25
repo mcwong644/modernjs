33e0930717c09b64d3361722710c705dadfee503
mcwong644 <mcwong644@gmail.com>
2023-08-25 15:55:13 +0800

    aug25b

---
+++ hello.c
@@ -0,0 +1,12 @@
+// C program showing the header file including
+// standard input-output header file
+ 
+#include <stdio.h>
+ 
+int main()
+{
+ 
+    // "printf()" belongs to stdio.h
+    printf("hello world");
+    return 0;
+}
