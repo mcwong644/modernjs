1d6265a38dd5d8bb7620e346264bb206d0aab206
mcwong644 <mcwong644@gmail.com>
2023-10-02 17:57:09 +0800

    Oct02

--- hello2.c
+++ hello2.c
@@ -1,7 +1,21 @@
+// https://youtu.be/zuegQmMdy8M?si=S6ARQVSJWf_k6X8F
+// Pointers in C / C++ [Full Course]
+// freeCodeCamp.org. stop 
+
+
+
 #include <stdio.h>
 
 int main(){
 
     printf("hello world 2\n");
+    int a = 1025;
+    unsigned *p;
+    p = &a;
+    printf("size of integer is %u bytes\n",sizeof(int));
+    printf("address = %d, value = %d\n",p,*p);
+    p += 1;
+    printf("address = %d, value = %d\n",p,*p);
+    
     return 0;
 }
